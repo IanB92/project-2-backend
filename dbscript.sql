440af2b465665f303f0bee0cdded7229d132dded
Joshua Cookhorne <joshua.cookhorne@revature.net>
2022-03-18 10:43:48 -0400

    quick changes

--- dbscript.sql
+++ dbscript.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE project_1;
+CREATE DATABASE project_2;
 
-\c project_1;
+\c project_2;
 
 CREATE TABLE employee_details(employee_id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY, first_name varchar(25), last_name varchar(25), address varchar(25), contact varchar(12), email varchar(25), username varchar(25), password varchar(25));
 
